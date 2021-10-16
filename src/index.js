ecebde80df88f1bb5072836caf30cda1339ac570
J-151 <januruparel151@gmail.com>
2021-10-16 20:15:36 +0530

    Digital Clock Assignment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import Style from "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  const [timeEven, settimeEven] = useState(0);
+  function toggle() {
+    settimeEven(!timeEven);
+  }
+  useEffect(() => {
+    setInterval(() => setTime(new Date()), 1000);
+    var interval = null;
+    if (time % 2 !== 0) {
+      settimeEven(1);
+      interval = setInterval(
+        () => settimeEven((seconds) => (seconds === 1 ? 0 : 1)),
+        1000
+      );
+    }
+    toggle();
+  }, []);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div
+      style={{
+        background: timeEven ? "black" : "white",
+        color: timeEven ? "white" : "black"
+      }}
+      className="App"
+    >
+      <h2>
+        {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      </h2>
+    </div>
+  );
+};
+
+render(<Clock />, document.getElementById("root"));
